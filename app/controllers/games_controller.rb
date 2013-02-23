f1d06316ffc7760dfedabe151fa34fbacf1fc6b6
Tiago de Assis Gonçalves <tlegiao@gmail.com>
2013-02-23 15:25:48 -0300

    Correções ao lançar horas

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(params[:game])
     @game.player1_id = current_player.id
+    @game.league = League.first
     @game.save
     
     respond_to do |format|
