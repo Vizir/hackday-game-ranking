05717941184998c1d66f6e56bc2f04efe6b92290
Fabricio Ferrari de Campos <ffc.fabricio@gmail.com>
2013-02-23 17:07:32 -0300

    Insercaoao de resultado: validacao quando nao historico

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,6 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(params[:game])
     @game.player1_id = current_player.id
-    @game.league = League.first
     @game.save
     
     respond_to do |format|
