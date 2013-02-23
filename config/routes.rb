f01abdccae42f077f92a097bd979747b4bfd2d98
apantaliao <andre0610@gmail.com>
2013-02-23 10:18:24 -0300

    criado Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HackdayGameRanking::Application.routes.draw do
+  devise_for :players
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -44,11 +46,11 @@ HackdayGameRanking::Application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   end
+  #   endcontro
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'players#index'
 
   # See how all your routes lay out with "rake routes"
 
