544d4c09e21850c45dcb306ba525d6fc38214f99
apantaliao <andre0610@gmail.com>
2013-02-23 11:04:32 -0300

    player: adicionado modelo

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,7 +6,7 @@ class Player < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :name
   # attr_accessible :title, :body
 
 end
