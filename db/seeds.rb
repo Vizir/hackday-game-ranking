97661711573eaef45241b99efe33dfa7ae2cf066
Ricardo <ricardo@vizir.com.br>
2013-02-23 15:52:49 -0300

    Criação do avatar do profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@
 league = League.create name: "Vizir League"
 
 Player.create [
-  {email: 'diego@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Diego Nakamashi', username: 'nakamashi'},
-  {email: 'fabricio@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Fabricio Campos', username: 'fabricio'},
-  {email: 'antonio@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Antonio Anderson', username: 'antonio'},
-  {email: 'tiago@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Tiago de Assis Gonçalves', username: 'tiago'},
-  {email: 'vitor@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Vitor Margis', username: 'vitor'},
-  {email: 'andre@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'André Pantalião', username: '12345678'},
-  {email: 'ricardo.georgel@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Ricardo Georgel', username: 'argentino'},
-  {email: 'david@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'David Lojudice Sobrinho', username: 'david'},
-  {email: 'daniel@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Daniel Arthaud', username: 'daniel'},
-  {email: 'paulo@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Paulo Felisbino', username: 'paulo'},
-  {email: 'ricardo@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Ricardo Zambelli', username: 'zambelli'}
+  {email: 'diego@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Diego Nakamashi', username: 'nakamashi', pictureUrl: ''},
+  {email: 'fabricio@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Fabricio Campos', username: 'fabricio', pictureUrl: ''},
+  {email: 'antonio@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Antonio Anderson', username: 'antonio', pictureUrl: ''},
+  {email: 'tiago@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Tiago de Assis Gonçalves', username: 'tiago', pictureUrl: ''},
+  {email: 'vitor@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Vitor Margis', username: 'vitor', pictureUrl: ''},
+  {email: 'andre@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'André Pantalião', username: '12345678', pictureUrl: ''},
+  {email: 'ricardo.georgel@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Ricardo Georgel', username: 'argentino', pictureUrl: ''},
+  {email: 'david@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'David Lojudice Sobrinho', username: 'david', pictureUrl: ''},
+  {email: 'daniel@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Daniel Arthaud', username: 'daniel', pictureUrl: ''},
+  {email: 'paulo@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Paulo Felisbino', username: 'paulo', pictureUrl: ''},
+  {email: 'ricardo@vizir.com.br', password: '12345678', password_confirmation: '12345678', name: 'Ricardo Zambelli', username: 'zambelli', pictureUrl: 'http://profile.ak.fbcdn.net/hprofile-ak-snc7/c20.20.256.256/s160x160/300271_184313231640090_4762926_n.jpg'}
 ]
 
 #Criação de rankings
