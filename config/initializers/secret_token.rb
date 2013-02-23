6c01eb1862564326a2d424fcb0ec7cefeb9fe22b
Fabricio Ferrari de Campos <ffc.fabricio@gmail.com>
2013-02-22 23:15:48 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackdayGameRanking::Application.config.secret_token = '92db7ef577775db6f30746918590f86083a9fd93c0e6fe5eadecaf8057b545d6efc0cf1cb27a1e014572e385f372a3e2d4017a132cbb6e79c7fd2e771e14597c'
