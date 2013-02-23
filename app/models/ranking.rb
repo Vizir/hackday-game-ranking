46d370a817c534a05d25e7f64589fe47a9900018
Tiago de Assis Gonçalves <tlegiao@gmail.com>
2013-02-23 14:50:16 -0300

    Criado metodos para atualizar a posição do ranking ao lançar novos resultados.

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -4,4 +4,12 @@ class Ranking < ActiveRecord::Base
   belongs_to :player
   attr_accessible :position, :player, :league, :row
 
+
+  def self::valid_for_ranking?(player_1, player_2)
+    row_player_1 = Ranking.where(player_id: player_1).first.row
+    row_player_2 = Ranking.where(player_id: player_2).first.row
+
+    row_player_1 == row_player_2 or row_player_1 == row_player_2 + 1 or row_player_1 == row_player_2 - 1
+  end
+
 end
