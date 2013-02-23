0889ba18eb6762c875790fd5b4408d8a80c29ea3
apantaliao <andre0610@gmail.com>
2013-02-23 15:25:05 -0300

    devise: criada as views

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -7,12 +7,19 @@ class Timeline < ActiveRecord::Base
   validates_presence_of :league, :message
 
   def self.create_message_based_on_game (game)
-    Timeline.create :message => "@#{game.winner[:username]} ganhou de @#{game.loser[:username]}.", 
+    game_message (game.player1_score - game.player2_score)
+    Timeline.create :message => "@#{game.player1.username} #{game.player1_score} X #{game.player2_score}  @#{game.player2.username}.", 
                     :league => game.league, :game => game
   end
 
   def self.create_message_based_on_ranking (ranking)
     Timeline.create :message => "@#{ranking.player.username} entrou no ranking.", 
                     :league => ranking.league
+  end
+
+private
+  def game_message(score)
+    
+
   end
 end
